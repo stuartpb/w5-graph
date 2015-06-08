fb80c1359d5b7a6c8c170116752e1c35a6f075cf
Stuart P. Bentley <stuart@testtrack4.com>
2015-06-08 11:03:04 +0000

    Rough first swing with Sigma and my own connections

---
+++ vend.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+vendor () {
+  cp "./node_modules/$1" "./vendor/${1##*/}"
+}
+
+npm install
+mkdir ./vendor
+vendor js-yaml/dist/js-yaml.js
+vendor sigma/build/sigma.min.js
+vendor sigma/build/plugins/sigma.layout.forceAtlas2.min.js
+vendor whatwg-fetch/fetch.js
